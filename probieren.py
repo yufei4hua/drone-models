8a6858c22494d3af3dda5ddf79cafa95350656f1
Martin Schuck <martin.schuck@tum.de>
2025-02-04 18:53:22 +0100

    Add .gitignore. Remove generic quat2euler.

--- probieren.py
+++ probieren.py
@@ -7,37 +7,37 @@ import lsy_models.rotation as rot
 
 # For testing
 quats = [
-        [1, 1, 1, 1],
-        [0, 0, 0, -1],
-        [0.5, 0, 0, 1],
-        [0, 0.5, 0, 1],
-        [0, 0, 0.5, 1],
-        [1, 1, 1, 1],
-        [0, 0, 0, -1],
-        [0.5, 0, 0, 1],
-        [0, 0.5, 0, 1],
-        [0, 0, 0.5, 1],
-        [1, 1, 1, 1],
-        [0, 0, 0, -1],
-        [0.5, 0, 0, 1],
-        [0, 0.5, 0, 1],
-        [0, 0, 0.5, 1],
-        [1, 1, 1, 1],
-        [0, 0, 0, -1],
-        [0.5, 0, 0, 1],
-        [0, 0.5, 0, 1],
-        [0, 0, 0.5, 1],
-        [1, 1, 1, 1],
-        [0, 0, 0, -1],
-        [0.5, 0, 0, 1],
-        [0, 0.5, 0, 1],
-        [0, 0, 0.5, 1],
-        [1, 1, 1, 1],
-        [0, 0, 0, -1],
-        [0.5, 0, 0, 1],
-        [0, 0.5, 0, 1],
-        [0, 0, 0.5, 1],
-        ]
+    [1, 1, 1, 1],
+    [0, 0, 0, -1],
+    [0.5, 0, 0, 1],
+    [0, 0.5, 0, 1],
+    [0, 0, 0.5, 1],
+    [1, 1, 1, 1],
+    [0, 0, 0, -1],
+    [0.5, 0, 0, 1],
+    [0, 0.5, 0, 1],
+    [0, 0, 0.5, 1],
+    [1, 1, 1, 1],
+    [0, 0, 0, -1],
+    [0.5, 0, 0, 1],
+    [0, 0.5, 0, 1],
+    [0, 0, 0.5, 1],
+    [1, 1, 1, 1],
+    [0, 0, 0, -1],
+    [0.5, 0, 0, 1],
+    [0, 0.5, 0, 1],
+    [0, 0, 0.5, 1],
+    [1, 1, 1, 1],
+    [0, 0, 0, -1],
+    [0.5, 0, 0, 1],
+    [0, 0.5, 0, 1],
+    [0, 0, 0.5, 1],
+    [1, 1, 1, 1],
+    [0, 0, 0, -1],
+    [0.5, 0, 0, 1],
+    [0, 0.5, 0, 1],
+    [0, 0, 0.5, 1],
+]
 # quats /= np.linalg.norm(quats) # normalization
 
 for q in quats:
@@ -81,3 +81,22 @@ t1 = time.perf_counter()
 R.from_quat(np.array(quats)).as_euler("xyz")
 t2 = time.perf_counter()
 print(f"time for scipy quat2euler 2D: {(t2-t1)*1000:.3f}ms")
+
+# Using timeit for more accurate benchmarking
+import timeit
+
+# XP quat2euler 1D
+t = timeit.timeit(lambda: rot.quat2euler(np.array(quats[0]), "xyz"), number=1000)
+print(f"time for xp quat2euler 1D (avg of 1000): {(t/1000)*1000:.3f}ms")
+
+# XP quat2euler 2D
+t = timeit.timeit(lambda: rot.quat2euler(np.array(quats), "xyz"), number=1000)
+print(f"time for xp quat2euler 2D (avg of 1000): {(t/1000)*1000:.3f}ms")
+
+# Scipy quat2euler 1D
+t = timeit.timeit(lambda: R.from_quat(np.array(quats[0])).as_euler("xyz"), number=1000)
+print(f"time for scipy quat2euler 1D (avg of 1000): {(t/1000)*1000:.3f}ms")
+
+# Scipy quat2euler 2D
+t = timeit.timeit(lambda: R.from_quat(np.array(quats)).as_euler("xyz"), number=1000)
+print(f"time for scipy quat2euler 2D (avg of 1000): {(t/1000)*1000:.3f}ms")
