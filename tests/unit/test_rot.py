5713cb2a56f098e86513dd5bb6f91935795c324c
rather1337 <marcel.rath@gmx.de>
2025-02-05 15:20:05 +0100

    Added test for rot_from_quat

--- tests/unit/test_rot.py
+++ tests/unit/test_rot.py
@@ -1,15 +1,14 @@
 """Testing the selfimplemented rotations against scipy rotations."""
 
-import jax.numpy as jnp
+import jax.numpy as jp
 import numpy as np
 import pytest
-from scipy.spatial.transform import Rotation as R
 
-import lsy_models.rotation as rot
+import lsy_models.rotation as R
 
 tol = 1e-6 # Since Jax by default works with 32 bit, the precision is worse
 
-def create_uniform_quats(n=10, range=10) -> list:
+def create_uniform_quats(n=100, range=10) -> list:
     """Creates an (n, 4) list with random quaternions."""
     # larger range because the function should be able to handle wrong length quaternions
     quats = np.random.uniform(-np.array([1,1,1,1])*range, np.array([1,1,1,1])*range, size=(n,4)) 
@@ -23,36 +22,22 @@ def create_lock_quats() -> list:
     return quats
 
 @pytest.mark.unit
-def test_quat2euler1D():
+def test_rot_from_quat():
     """Testing Quaternion to Euler angle with individual arrays."""
     quats = create_uniform_quats()
     quats.extend(create_lock_quats())
 
+    # Testing individual
     for q in quats:
-        rotation = R.from_quat(q)
-        assert np.allclose(rotation.as_euler("xyz"), rot.quat2euler(np.array(q), "xyz"))
-        assert np.allclose(rotation.as_euler("XYZ"), rot.quat2euler(np.array(q), "XYZ"))
-        assert jnp.allclose(rotation.as_euler("xyz"), rot.quat2euler(jnp.array(q), "xyz"), atol=tol)
-        assert jnp.allclose(rotation.as_euler("XYZ"), rot.quat2euler(jnp.array(q), "XYZ"), atol=tol)
+        R.from_quat(np.array(q))
+        R.from_quat(jp.array(q))
+
+    # Testing batched
+    R.from_quat(np.array(quats))
+    R.from_quat(jp.array(quats))
 
 
 @pytest.mark.unit
-def test_quat2euler2D():
-    """Testing Quaternion to Euler angle with batched arrays."""
-    quats = create_uniform_quats()
-    # quats /= np.linalg.norm(quats) # normalization
-
-    rotations = R.from_quat(quats)
-
-    assert np.allclose(rotations.as_euler("xyz"), rot.quat2euler(np.array(quats), "xyz"))
-    assert np.allclose(rotations.as_euler("XYZ"), rot.quat2euler(np.array(quats), "XYZ"))
-    assert jnp.allclose(rotations.as_euler("xyz"), rot.quat2euler(jnp.array(quats), "xyz"), atol=tol)
-    assert jnp.allclose(rotations.as_euler("XYZ"), rot.quat2euler(jnp.array(quats), "XYZ"), atol=tol)
-
-
-a = create_uniform_quats()
-b = create_lock_quats()
-
-print(a,b)
-a.extend(b)
-print(a)
+def test_rot_from_euler():
+    """Testing Quaternion to Euler angle with individual arrays."""
+    ... # TODO
